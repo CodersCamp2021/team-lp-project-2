efa9baf7c2c68ba8de8fd18bb5445e1a44b8e9d4
pastepi <pawel.st123@gmail.com>
2022-01-21 23:57:11 +0100

    Added example navbar to test Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { Text } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DummyHome from './components/DummyHome';
 import DummyStore from './components/DummyStore';
+import { Link } from 'react-router-dom';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
 
 function App() {
   return (
     <Router>
-      <Text>Hello world</Text>
-      <DummyHome />
-      <DummyStore />
+      <Breadcrumb separator={false}>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/">
+            Home
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/store">
+            Store
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Routes>
+        <Route path="/" element={<DummyHome />} />
+        <Route path="/store" element={<DummyStore />} />
+      </Routes>
     </Router>
   );
 }
