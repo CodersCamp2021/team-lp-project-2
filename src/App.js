640f1d289f7cb4d0aa14fcd45b6cc6541102b559
pastepi <pawel.st123@gmail.com>
2022-01-21 20:54:58 +0100

    Initial commit. Moved Router to App.js, changed App.js directory

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Text>Hello world</Text>
+    </Router>
+  );
+}
+
+export default App;
