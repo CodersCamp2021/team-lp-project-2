232af3be7fa638b4475a0d7abd4c357375b09d53
Maciek <maciek.duzy.11@gmail.com>
2022-01-22 16:28:24 +0100

    event listener removed

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -3,23 +3,12 @@ import React, { useEffect, useState } from 'react';
 import Logo from './Logo';
 import MenuIcon from './MenuIcon';
 import MenuLinks from './MenuLinks';
-import StoreActions from './StoreActions';
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const isMobileView = windowWidth < 750;
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      setWindowWidth(window.innerWidth);
-    });
-
-    console.log(windowWidth);
-  });
 
   return (
     <Flex
@@ -33,11 +22,9 @@ const Navbar = () => {
       height="80px"
     >
       <Logo />
-      <MenuLinks isMobileView={isMobileView} />
-      {!isMobileView && <StoreActions />}
-      {isMobileView && (
-        <MenuIcon isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
-      )}
+      <SearchBar isMenuOpen={isMenuOpen} />
+      <MenuLinks isMenuOpen={isMenuOpen} />
+      <MenuIcon isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </Flex>
   );
 };
