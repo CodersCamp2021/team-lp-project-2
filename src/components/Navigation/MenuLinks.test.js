00210cdf93b50efadcadc4e974ea69c85a3e547d
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-01-23 16:16:16 +0100

    First Tests add

---
+++ src/components/Navigation/MenuLinks.test.js
@@ -0,0 +1,14 @@
+import {render} from '@testing-library/react'
+import MenuLinks from './MenuLinks';
+
+test("Render MenuLinks component corectly", () => {
+    render(<MenuLinks />)
+});
+
+test("Home and Store Links are clickable", () => {
+
+});
+
+test("Clicking on store opens the correct tab", () => {
+
+});
---
+++ src/components/Navigation/SearchBar.test.js
@@ -0,0 +1,12 @@
+import {render} from '@testing-library/react'
+import SearchBar from './SearchBar';
+
+test("Render SearchBar component corectly", () => {
+    render(<SearchBar />);
+    //const searchBar = document.getElementsByTagName("SearchBar");
+});
+
+test("Click on the search icon opens the correct tab", () => {
+
+});
+
