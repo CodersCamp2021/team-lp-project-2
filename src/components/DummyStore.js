128cc1695f25e011a3f538fc46545560bc620476
pastepi <pawel.st123@gmail.com>
2022-01-24 00:05:39 +0100

    Added fetching from firebase to DummyStore.js

--- src/components/DummyStore.js
+++ src/components/DummyStore.js
@@ -2,12 +2,37 @@ import { Center, Stack, Flex, Button } from '@chakra-ui/react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import ProductList from './ProductList';
 import DummyProductDisplay from './DummyProductDisplay';
-import { useState, createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import app from '../firebase';
 
 export const CategoryContext = createContext(undefined);
 
 const DummyStore = () => {
   const [category, setCategory] = useState(undefined);
+  const [products, setProducts] = useState([]);
+
+  const db = getFirestore(app);
+
+  const getAllProducts = async () => {
+    let fetchedProducts = [];
+    const booksRef = collection(db, 'products');
+    const snapshot = await getDocs(booksRef);
+
+    snapshot.docs.forEach((doc) => {
+      const data = doc.data();
+      fetchedProducts.push({ ...data, id: doc.id });
+    });
+    setProducts(fetchedProducts);
+  };
+
+  useEffect(() => {
+    try {
+      getAllProducts();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
   const updateCategory = (newCategory) => {
     setCategory(newCategory);
@@ -24,13 +49,17 @@ const DummyStore = () => {
           <NavLink to=".">All products</NavLink>
           <NavLink to="./CPU">CPU</NavLink>
           <NavLink to="./GPU">GPU</NavLink>
+          <NavLink to="./RAM">RAM</NavLink>
           <NavLink to="./Monitor">Monitor</NavLink>
         </Stack>
 
         <CategoryContext.Provider value={updateCategory}>
           <Routes>
-            <Route path="/" element={<ProductList />} />
-            <Route path="/:category" element={<ProductList />} />
+            <Route path="/" element={<ProductList products={products} />} />
+            <Route
+              path="/:category"
+              element={<ProductList products={products} />}
+            />
             <Route
               path="/product/:productId"
               element={<DummyProductDisplay />}
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,39 +1,22 @@
 import { Link, useParams } from 'react-router-dom';
 import { SimpleGrid, Box, Text, Flex, Heading } from '@chakra-ui/react';
 import { TEST_ITEMS } from '../DummyItems';
-import { useContext, useEffect } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { CategoryContext } from './DummyStore';
-import queryString from 'query-string';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import app from '../firebase';
+// import queryString from 'query-string';
 
-const ProductList = () => {
+const ProductList = ({ products }) => {
   let { category } = useParams();
   const updateCategory = useContext(CategoryContext);
-  const queryParams = queryString.parse(window.location.search); // list of all query params as {key: value}
-  const db = getFirestore(app);
-
-  const getAllProducts = () => {
-    const booksRef = collection(db, 'products');
-    getDocs(booksRef).then((snapshot) => {
-      let product = [];
-      snapshot.docs.forEach((doc) => {
-        const data = doc.data();
-        product.push({ ...data, id: doc.id });
-      });
-      return product;
-    });
-  };
+  // const queryParams = queryString.parse(window.location.search); // list of all query params as {key: value}
 
   const categoryFilter = () => {
     if (category) {
-      console.log(1);
-      return getAllProducts().filter(
+      return products.filter(
         (product) => product.type.toLowerCase() === category.toLowerCase(),
       );
     } else {
-      console.log(2);
-      return getAllProducts();
+      return products;
     }
   };
 
@@ -50,23 +33,24 @@ const ProductList = () => {
         justifyItems="center"
         alignItems="center"
       >
-        {categoryFilter().map((product) => (
-          <Link key={product.name} to={`/store/product/${product.name}`}>
-            <Box
-              p={5}
-              shadow="md"
-              borderWidth="1px"
-              width="200px"
-              height="160px"
-              textAlign="center"
-            >
-              <Heading fontSize="md">{product.name}</Heading>
-              <Text fontSize="md">${product.price}</Text>
-              <Text fontSize="md">{product.brand}</Text>
-              {/* <Text fontSize="md">{product.details.brand}</Text> */}
-            </Box>
-          </Link>
-        ))}
+        {products.length > 0
+          ? categoryFilter().map((product) => (
+              <Link key={product.name} to={`/store/product/${product.name}`}>
+                <Box
+                  p={5}
+                  shadow="md"
+                  borderWidth="1px"
+                  width="200px"
+                  height="160px"
+                  textAlign="center"
+                >
+                  <Heading fontSize="md">{product.name}</Heading>
+                  <Text fontSize="md">${product.price}</Text>
+                  <Text fontSize="md">{product.details.brand}</Text>
+                </Box>
+              </Link>
+            ))
+          : 'Loading...'}
       </SimpleGrid>
     </Flex>
   );
