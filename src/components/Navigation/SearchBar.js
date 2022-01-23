1f9b54bbab9e6afc12af3ce6cb725584ec4ed17e
Maciek <maciek.duzy.11@gmail.com>
2022-01-23 11:12:56 +0100

    add search params

--- src/components/Navigation/SearchBar.js
+++ src/components/Navigation/SearchBar.js
@@ -2,8 +2,20 @@ import { Flex, Input } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = ({ isMenuOpen }) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+
+    if (!name) return;
+
+    navigate(`/store?name=${name}`);
+  };
+
   return (
     <Flex
       display={{ base: isMenuOpen ? 'none' : 'flex', md: 'flex' }}
@@ -22,13 +34,20 @@ const SearchBar = ({ isMenuOpen }) => {
       pt={{ base: '40px', md: '0' }}
       pb={{ base: '40px', md: '0' }}
     >
-      <Input
-        maxWidth="500px"
-        minWidth="100px"
-        borderColor="blackAlpha.500"
-        focusBorderColor="blackAlpha.900"
+      <form onSubmit={handleSubmit}>
+        <Input
+          maxWidth="500px"
+          minWidth="100px"
+          borderColor="blackAlpha.500"
+          focusBorderColor="blackAlpha.900"
+        />
+      </form>
+      <SearchIcon
+        cursor="pointer"
+        position="relative"
+        right="36px"
+        boxSize="25px"
       />
-      <SearchIcon position="relative" right="36px" boxSize="25px" />
     </Flex>
   );
 };
