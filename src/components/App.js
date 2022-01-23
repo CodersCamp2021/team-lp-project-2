22c04b0add027d610c5f6656bc31d33d38fe1ad3
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-01-23 01:49:54 +0100

    added range slider

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import { Text } from '@chakra-ui/react';
+import Filters from './filters/Filters';
 
 function App() {
-  return <Text>Hello world</Text>;
+  return (
+    <>
+      <Text>Hello world</Text>
+      <Filters />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/filters/Filters.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Options from './Options';
+import {
+  Flex,
+  RangeSlider,
+  Text,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Divider,
+  NumberInput,
+  NumberInputField,
+  FormControl,
+  FormLabel,
+} from '@chakra-ui/react';
+
+export default function Filters({ products, category }) {
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(400);
+  const primaryMin = 0;
+  const primaryMax = 400;
+
+  return (
+    <Flex
+      w="15%"
+      bg="#f1f1f1"
+      direction="column"
+      justify="center"
+      align="flex-start"
+    >
+      <Text pl="2%" fontSize="2xl" fontWeight="bold">
+        Filters
+      </Text>
+      <Text p="3%" fontSize="xl">
+        Price
+      </Text>
+
+      <RangeSlider
+        alignSelf="center"
+        w="70%"
+        defaultValue={[minPrice, maxPrice]}
+        min={primaryMin}
+        max={primaryMax}
+        step={50}
+        onChangeEnd={(val) => {
+          setMinPrice(val[0]);
+          setMaxPrice(val[1]);
+        }}
+      >
+        <RangeSliderTrack bg="red.100">
+          <RangeSliderFilledTrack bg="tomato" />
+        </RangeSliderTrack>
+        <RangeSliderThumb boxSize={4} index={0} />
+        <RangeSliderThumb boxSize={4} index={1} />
+      </RangeSlider>
+      <Text pl="3%">Minimum price: {minPrice}</Text>
+      <Text pl="3%">Maximum price: {maxPrice}</Text>
+      <Divider />
+      <Options props={category} />
+    </Flex>
+  );
+}
