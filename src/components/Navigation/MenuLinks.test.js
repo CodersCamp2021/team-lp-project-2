d009b5f036084b5bb4c16e29a370f811aa7e13e8
Maciek <maciek.duzy.11@gmail.com>
2022-01-24 17:08:52 +0100

    test corrected

--- src/components/Navigation/MenuLinks.test.js
+++ src/components/Navigation/MenuLinks.test.js
@@ -1,32 +1,38 @@
-import {render, screen, fireEvent} from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react';
 import MenuLinks from './MenuLinks';
 import { BrowserRouter } from 'react-router-dom';
 
-test("Render Home MenuLinks component corectly", () => {
-    render(
-        <BrowserRouter>
-            <MenuLinks />
-        </BrowserRouter>)
-    const home = screen.getByText('Home');
-    expect(home).toBeInTheDocument();
+test('Render Home MenuLinks component corectly', () => {
+  render(
+    <BrowserRouter>
+      <MenuLinks />
+    </BrowserRouter>,
+  );
+  const home = screen.getByText('Home');
+  expect(home).toBeInTheDocument();
 });
 
-test("Render Store MenuLinks component corectly", () => {
-    render(
-        <BrowserRouter>
-            <MenuLinks />
-        </BrowserRouter>)
-    const store = screen.getByText('Store');
-    expect(store).toBeInTheDocument();
+test('Render Store MenuLinks component corectly', () => {
+  render(
+    <BrowserRouter>
+      <MenuLinks />
+    </BrowserRouter>,
+  );
+  const store = screen.getByText('Store');
+  expect(store).toBeInTheDocument();
 });
 
-test("Home and Store Links are clickable", () => {
-    render(
-        <BrowserRouter>
-            <MenuLinks />
-        </BrowserRouter>);
-    const home = screen.getByText('Store');
-    const store = screen.getByText('Store');
-    expect(fireEvent.click(home));
-    expect(fireEvent.click(store));
+test('store Link changes url', () => {
+  global.window = { location: { pathname: '/' } };
+
+  render(
+    <BrowserRouter>
+      <MenuLinks />
+    </BrowserRouter>,
+  );
+  const store = screen.getByTestId('store');
+
+  fireEvent.click(store);
+
+  expect(global.window.location.pathname).toEqual('/store');
 });
