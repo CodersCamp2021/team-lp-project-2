ff270defdd09b359db480074b2039e1b139646e1
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-01-24 18:36:02 +0100

    correct tests

--- src/components/Categories.test.js
+++ src/components/Categories.test.js
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
 test("render component", () => {
-const {getByText}  = render(<Categories />)
-expect(getByText('Products')).toBeInTheDocument();
-expect(getByText('Memory')).toBeInTheDocument();
+const view  = render(<Categories />);
+expect(view('Products')).toBeInTheDocument();
+expect(view('Memory')).toBeInTheDocument();
 })
+
