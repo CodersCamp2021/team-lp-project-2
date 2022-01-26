0dd8f898b5f7f537211edc45514fc9eae458cf57
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-01-26 21:28:37 +0100

    add next tests

--- src/components/Categories.test.js
+++ src/components/Categories.test.js
@@ -1,9 +1,27 @@
-import Categories from './Categories';
-import { render } from '@testing-library/react';
+import { Categories, Link } from './Categories';
+import { render, fireEvent } from '@testing-library/react';
 
-test("render component correctly", () => {
-const { screen }  = render(<Categories />);
-const renderText = screen.getByRole('link');
-expect(renderText).toBeInTheDocument();
-})
 
+test('render component correctly', () => {
+  const { screen } = render(
+    <Categories>
+      <Link />
+    </Categories>,
+  );
+  const renderText = screen.getByRole('link');
+  expect(renderText).toBeInTheDocument();
+});
+
+test('product Link changes url', () => {
+  global.window = { location: { pathname: null } };
+  const { screen } = render(
+    <Categories>
+      <Link />
+    </Categories>,
+  );
+  const cpus = screen.getByText('CPUs');
+
+  fireEvent.click(cpus);
+
+  expect(global.window.location.pathname).toEqual('/store/cpu');
+});
