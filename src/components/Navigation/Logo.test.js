d7b415e7debb5933d30baa164f14c4978c525b63
Burbinox <hubert.grobelny@interia.pl>
2022-01-26 22:42:54 +0100

    fix old test and add test for router path

--- src/components/Navigation/Logo.test.js
+++ src/components/Navigation/Logo.test.js
@@ -1,8 +1,13 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import Logo from './Logo';
+import { BrowserRouter } from 'react-router-dom';
 
 test('Renders Logo Box component corectly', () => {
-  render(<Logo />);
+  render(
+    <BrowserRouter>
+      <Logo />
+    </BrowserRouter>,
+  );
   const img = screen.getByAltText('Logo');
   expect(img).toBeInTheDocument();
 });
@@ -10,7 +15,11 @@ test('Renders Logo Box component corectly', () => {
 test('click on Logo image changes url', () => {
   global.window = { location: { pathname: null } };
 
-  render(<Logo />);
+  render(
+    <BrowserRouter>
+      <Logo />
+    </BrowserRouter>,
+  );
   const logo = screen.getByAltText('Logo');
 
   fireEvent.click(logo);
