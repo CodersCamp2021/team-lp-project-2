dd57f6daaf2223f56a52a80387dd24ea7cd8a241
Burbinox <hubert.grobelny@interia.pl>
2022-01-27 21:40:56 +0100

    add test for every category

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,13 +1,67 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('click on Store image changes url', () => {
+test('click on CPU category changes url', () => {
   global.window = { location: { pathname: null } };
 
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
   const store = screen.getByText('Store');
-
   fireEvent.click(store);
+  const cpu = screen.getByText('CPU');
+  fireEvent.click(cpu);
 
-  expect(global.window.location.pathname).toEqual('/store');
+  expect(global.window.location.pathname).toEqual('/store/CPU');
+});
+
+test('click on GPU category changes url', () => {
+  global.window = { location: { pathname: null } };
+
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const store = screen.getByText('Store');
+  fireEvent.click(store);
+  const gpu = screen.getByText('GPU');
+  fireEvent.click(gpu);
+
+  expect(global.window.location.pathname).toEqual('/store/GPU');
+});
+
+test('click on RAM category changes url', () => {
+  global.window = { location: { pathname: null } };
+
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const store = screen.getByText('Store');
+  fireEvent.click(store);
+  const ram = screen.getByText('RAM');
+  fireEvent.click(ram);
+
+  expect(global.window.location.pathname).toEqual('/store/RAM');
+});
+
+test('click on Monitor category changes url', () => {
+  global.window = { location: { pathname: null } };
+
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const store = screen.getByText('Store');
+  fireEvent.click(store);
+  const monitor = screen.getByText('Monitor');
+  fireEvent.click(monitor);
+
+  expect(global.window.location.pathname).toEqual('/store/Monitor');
 });
