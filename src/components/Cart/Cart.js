cc7e4db401d8addfcbf3f11b8bc1bb674f48d250
Maciek <maciek.duzy.11@gmail.com>
2022-01-29 12:26:09 +0100

    add CartBackground

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,27 @@
-import { Box, Button } from '@chakra-ui/react';
 import React from 'react';
+import { Box, Button, Text } from '@chakra-ui/react';
+import { FaShoppingCart } from 'react-icons/fa';
+import CartBackground from './CartBackground';
 
 const Cart = ({ isCartOpen, closeCart }) => {
   return (
-    <Box
-      height="100vh"
-      width="600px"
-      bg="white"
-      position="fixed"
-      top="0"
-      right="-1000px"
-      transform={isCartOpen ? 'translateX(-1000px)' : ''}
-      transition="transform ease-in-out .2s"
-    >
-      <Button onClick={closeCart}>Close cart</Button>
-    </Box>
+    <>
+      <CartBackground isCartOpen={isCartOpen} closeCart={closeCart} />
+      <Box
+        height="100vh"
+        width="600px"
+        bg="white"
+        position="fixed"
+        top="0"
+        right="-1000px"
+        transform={isCartOpen ? 'translateX(-1000px)' : ''}
+        transition="transform ease-in-out .4s .3s"
+      >
+        <FaShoppingCart />
+        <Text>Cart</Text>
+        <Button onClick={closeCart}>Close cart</Button>
+      </Box>
+    </>
   );
 };
 
