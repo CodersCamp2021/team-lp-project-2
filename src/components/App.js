55ee1294f616afb3134a26222bca82d842165ffc
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-01-29 13:04:30 +0100

    Fotter update

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import Store from './Store';
 import Navbar from './Navigation/Navbar';
 import Footer from './HomePage/Footer';
 
-
 function App() {
   return (
     <>
--- src/components/HomePage/Footer.js
+++ src/components/HomePage/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Flex, Box } from '@chakra-ui/react';
+import { FaFacebookMessenger, FaCreditCard, FaExchangeAlt, FaShippingFast } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -8,11 +9,27 @@ const Footer = () => {
     align="center"
     justify="space-between"
     width="100%"
-    height="80px">
-      <Box>Free shipping</Box>
-      <Box>90 days return</Box>
-      <Box>Secure payments</Box>
-      <Box>24/7 dedicated support</Box>
+    height="80px"
+    gap="40px"
+    background="white.500"
+    px="80px"
+    >
+    <Box display="flex" gap="10px">
+        <FaShippingFast size="25px" />
+        Free shipping
+    </Box>
+    <Box display="flex" gap="10px">
+        <FaExchangeAlt size="25px" />
+        90 days return
+    </Box>
+    <Box display="flex" gap="10px">
+        <FaCreditCard size="25px" />
+        Secure payments
+    </Box>
+    <Box display="flex" gap="10px">
+        <FaFacebookMessenger size="25px" />
+        24/7 dedicated support
+    </Box>
   </Flex>);
 };
 
