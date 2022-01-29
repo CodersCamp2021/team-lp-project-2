1ce3ef5dbd7fbff9b8a3f591e2d9a62303dd4e25
Maciek <maciek.duzy.11@gmail.com>
2022-01-29 12:45:04 +0100

    add close icon & header

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, Button, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { FaShoppingCart } from 'react-icons/fa';
 import CartBackground from './CartBackground';
+import { CloseIcon } from '@chakra-ui/icons';
 
 const Cart = ({ isCartOpen, closeCart }) => {
   return (
@@ -13,13 +14,31 @@ const Cart = ({ isCartOpen, closeCart }) => {
         bg="white"
         position="fixed"
         top="0"
+        padding="20px"
         right="-1000px"
         transform={isCartOpen ? 'translateX(-1000px)' : ''}
         transition="transform ease-in-out .4s .3s"
       >
-        <FaShoppingCart />
-        <Text>Cart</Text>
-        <Button onClick={closeCart}>Close cart</Button>
+        <CloseIcon
+          onClick={closeCart}
+          boxSize="30px"
+          position="absolute"
+          top="30px"
+          right="30px"
+          cursor="pointer"
+        />
+
+        <Flex
+          justify="center"
+          align="center"
+          pb="20px"
+          borderBottom="1px solid black"
+        >
+          <FaShoppingCart size="35px" />
+          <Text fontSize="3xl" ml="10px">
+            Cart
+          </Text>
+        </Flex>
       </Box>
     </>
   );
