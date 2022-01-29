39cb902ed8d8c3c0059123d7a701f7b55f4b9473
Maciek <maciek.duzy.11@gmail.com>
2022-01-29 11:50:48 +0100

    add state for cart status

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,28 @@ import { Route, Routes } from 'react-router-dom';
 import DummyHome from './DummyHome';
 import Store from './Store';
 import Navbar from './Navigation/Navbar';
+import Cart from './Cart/Cart';
+import { useState } from 'react';
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(true);
+
+  const handleOpenCart = () => {
+    setIsCartOpen(true);
+  };
+
+  const handleCloseCart = () => {
+    setIsCartOpen(false);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar openCart={handleOpenCart} />
       <Routes>
         <Route path="/" element={<DummyHome />} />
         <Route path="/store/*" element={<Store />} />
       </Routes>
+      <Cart isCartOpen={isCartOpen} closeCart={handleCloseCart} />
     </>
   );
 }
