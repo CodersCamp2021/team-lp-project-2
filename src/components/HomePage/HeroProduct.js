696437e868648f576d68f6863f90667066746449
pastepi <pawel.st123@gmail.com>
2022-01-31 00:46:09 +0100

    Added image switching functionality to Carousel

--- src/components/HomePage/HeroProduct.js
+++ src/components/HomePage/HeroProduct.js
@@ -1,8 +1,23 @@
 import { Flex, Image, IconButton, ButtonGroup } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
 import ProductCarousel from './ProductCarousel';
+import { DummyProducts } from './DummyProducts';
+import ProductList from '../ProductList';
 
 const HeroProduct = () => {
+  const [active, setActive] = useState(0);
+
+  const handleIncrement = () => {
+    setActive((active) =>
+      active === DummyProducts.length - 1 ? active : active + 1,
+    );
+  };
+
+  const handleDecrement = () => {
+    setActive((active) => (active === 0 ? active : active - 1));
+  };
+
   return (
     <Flex flexDirection="column" alignItems="center" justifyContent="center">
       {/* <Image
@@ -11,7 +26,7 @@ const HeroProduct = () => {
         pb={8}
         src="https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/AMD%20Ryzen%205%203600X%2F1.jpg?alt=media"
       /> */}
-      <ProductCarousel />
+      <ProductCarousel active={active} />
       <ButtonGroup spacing={10} size="lg">
         <IconButton
           aria-label="View previous item"
@@ -19,6 +34,8 @@ const HeroProduct = () => {
           borderRadius="50%"
           bgGradient="linear-gradient(0deg, #DCDCDC 40%, rgba(255, 255, 255, 0.8) 100%)"
           icon={<ChevronLeftIcon />}
+          onClick={handleDecrement}
+          disabled={active === 0 ? true : false}
         />
         <IconButton
           aria-label="View next item"
@@ -26,6 +43,8 @@ const HeroProduct = () => {
           borderRadius="50%"
           bgGradient="linear-gradient(0deg, #DCDCDC 40%, rgba(255, 255, 255, 0.8) 100%)"
           icon={<ChevronRightIcon />}
+          onClick={handleIncrement}
+          disabled={active === DummyProducts.length - 1 ? true : false}
         />
       </ButtonGroup>
     </Flex>
--- src/components/HomePage/ProductCarousel.js
+++ src/components/HomePage/ProductCarousel.js
@@ -2,8 +2,7 @@ import { Flex, Image } from '@chakra-ui/react';
 import { useState } from 'react';
 import { DummyProducts } from './DummyProducts';
 
-const ProductCarousel = () => {
-  const [active, setActive] = useState(0);
+const ProductCarousel = ({ active }) => {
   const productList = DummyProducts;
   return (
     <div>
@@ -19,7 +18,7 @@ const ProductCarousel = () => {
           <Image
             display="none"
             width={10}
-            key={product.name}
+            key={product.name} // TODO: Change to ID with real products
             src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.images[0]}?alt=media`}
             alt={product.name}
             className={index === active ? 'active' : ''}
