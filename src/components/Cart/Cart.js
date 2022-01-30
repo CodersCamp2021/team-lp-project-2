748dca00056825c6e86350fd08dd10bb908f14f6
Maciek <maciek.duzy.11@gmail.com>
2022-01-30 13:57:37 +0100

    display products conditionally

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import CartBackground from './CartBackground';
 import CartHeader from './CartHeader';
 import CartSummary from './CartSummary';
 import Products from './Products';
+
+const dummyProducts = [
+  {
+    id: '1ZaZ9ciAQ1r5SQjgEb7J',
+    name: 'Intel Core i9-9900KF',
+    price: 276.32,
+    amount: 1,
+  },
+  {
+    id: '2E77PN30gTjkA4T8QjOM',
+    name: 'Corsair CMW32GX4M2E3200C16',
+    price: 59.99,
+    amount: 4,
+  },
+  {
+    id: '5cc6UzJixdCcUsUPoEkF',
+    name: 'SAMSUNG 23.5” CF396 Curved Computer Monitor',
+    price: 169.99,
+    amount: 1,
+  },
+  {
+    id: '6G5uJAppKfO0Aprucdx2',
+    name: 'AMD Ryzen 7 5800X',
+    price: 329.78,
+    amount: 6,
+  },
+];
 
 const Cart = ({ isCartOpen, closeCart }) => {
   return (
@@ -24,7 +51,14 @@ const Cart = ({ isCartOpen, closeCart }) => {
         <CartHeader closeCart={closeCart} />
 
         <Box bg="tomato" w="100%" h="90%" p={10} color="white">
-          <Products />
+          {/* dynamically display products or  info if products array is empty */}
+          {dummyProducts.length ? (
+            <Products products={dummyProducts} />
+          ) : (
+            <Flex justify="center">
+              <Text fontSize="3xl">The cart is empty</Text>
+            </Flex>
+          )}
           <CartSummary />
         </Box>
       </Box>
--- src/components/Cart/Products.js
+++ src/components/Cart/Products.js
@@ -2,37 +2,10 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 import SingleProduct from './SingleProduct';
 
-const dummyProducts = [
-  {
-    id: '1ZaZ9ciAQ1r5SQjgEb7J',
-    name: 'Intel Core i9-9900KF',
-    price: 276.32,
-    amount: 1,
-  },
-  {
-    id: '2E77PN30gTjkA4T8QjOM',
-    name: 'Corsair CMW32GX4M2E3200C16',
-    price: 59.99,
-    amount: 4,
-  },
-  {
-    id: '5cc6UzJixdCcUsUPoEkF',
-    name: 'SAMSUNG 23.5” CF396 Curved Computer Monitor',
-    price: 169.99,
-    amount: 1,
-  },
-  {
-    id: '6G5uJAppKfO0Aprucdx2',
-    name: 'AMD Ryzen 7 5800X',
-    price: 329.78,
-    amount: 6,
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   return (
     <Box bg="blue" w="100%" h="50%" p={10} color="white">
-      {dummyProducts.map((product) => {
+      {products.map((product) => {
         const { id, name, price, amount } = product;
         return (
           <SingleProduct key={id} name={name} price={price} amount={amount} />
