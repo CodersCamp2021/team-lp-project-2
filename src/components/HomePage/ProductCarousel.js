b49bc45335c30c8c26d073a30f78b9c8e3647958
pastepi <p.stepien@livechat.com>
2022-01-31 13:46:20 +0100

    Removed comments, adjusted image container height

--- src/components/HomePage/ProductCarousel.js
+++ src/components/HomePage/ProductCarousel.js
@@ -4,22 +4,7 @@ import { DummyProducts } from './DummyProducts';
 const ProductCarousel = ({ active }) => {
   const productList = DummyProducts;
   return (
-    <Flex
-      height="100%"
-      width="100%"
-      justifyContent="center"
-      alignItems="center"
-    >
-      {/* <Image
-          height="lg"
-          objectFit="contain"
-          pb={8}
-          src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${productList[active].images[0]}?alt=media`}
-          alt={productList[active].name}
-          transition="200ms opacity ease-in-out"
-          opacity={1}
-          position="relative"
-        /> */}
+    <Flex height="80%" width="100%" justifyContent="center" alignItems="center">
       {productList.map((product, index) => (
         <Image
           boxSize={{ base: 'xs', sm: 'md', md: 'lg' }}
@@ -28,7 +13,6 @@ const ProductCarousel = ({ active }) => {
           position="absolute"
           opacity={index === active ? 1 : 0}
           transition="300ms opacity ease-in-out 300ms"
-          // display="none"
           key={product.name} // TODO: Change to ID with real products
           src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.images[0]}?alt=media`}
           alt={product.name}
