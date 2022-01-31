d3c37ab01a90dc3bdce02549f6c4fa87c2a057a6
pastepi <p.stepien@livechat.com>
2022-01-31 13:34:31 +0100

    Added simple transitions for product carousel

--- src/components/HomePage/ProductCarousel.js
+++ src/components/HomePage/ProductCarousel.js
@@ -1,31 +1,41 @@
 import { Flex, Image } from '@chakra-ui/react';
-import { useState } from 'react';
 import { DummyProducts } from './DummyProducts';
 
 const ProductCarousel = ({ active }) => {
   const productList = DummyProducts;
   return (
-    <div>
-      <Image
-        height="lg"
-        objectFit="contain"
-        pb={8}
-        src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${productList[active].images[0]}?alt=media`}
-        alt={productList[active].name}
-      />
-      <Flex>
-        {productList.map((product, index) => (
-          <Image
-            display="none"
-            width={10}
-            key={product.name} // TODO: Change to ID with real products
-            src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.images[0]}?alt=media`}
-            alt={product.name}
-            className={index === active ? 'active' : ''}
-          />
-        ))}
-      </Flex>
-    </div>
+    <Flex
+      height="100%"
+      width="100%"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {/* <Image
+          height="lg"
+          objectFit="contain"
+          pb={8}
+          src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${productList[active].images[0]}?alt=media`}
+          alt={productList[active].name}
+          transition="200ms opacity ease-in-out"
+          opacity={1}
+          position="relative"
+        /> */}
+      {productList.map((product, index) => (
+        <Image
+          boxSize={{ base: 'xs', sm: 'md', md: 'lg' }}
+          objectFit="contain"
+          pb={8}
+          position="absolute"
+          opacity={index === active ? 1 : 0}
+          transition="300ms opacity ease-in-out 300ms"
+          // display="none"
+          key={product.name} // TODO: Change to ID with real products
+          src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.images[0]}?alt=media`}
+          alt={product.name}
+          className={index === active ? 'active' : ''}
+        />
+      ))}
+    </Flex>
   );
 };
 
