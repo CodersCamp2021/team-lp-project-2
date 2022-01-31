4ba18ea13c22ca998fface1dac0f987e3e23aa7c
pastepi <p.stepien@livechat.com>
2022-01-31 13:46:48 +0100

    Removed comments

--- src/components/HomePage/HeroProduct.js
+++ src/components/HomePage/HeroProduct.js
@@ -6,12 +6,6 @@ import { DummyProducts } from './DummyProducts';
 const HeroProduct = ({ active, increment, decrement }) => {
   return (
     <Flex flexDirection="column" alignItems="center" justifyContent="center">
-      {/* <Image
-        height="lg"
-        objectFit="contain"
-        pb={8}
-        src="https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/AMD%20Ryzen%205%203600X%2F1.jpg?alt=media"
-      /> */}
       <ProductCarousel active={active} />
       <ButtonGroup spacing={10} size="lg">
         <IconButton
