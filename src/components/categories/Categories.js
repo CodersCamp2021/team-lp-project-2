d5533d370ac598453d6c192615aefac28423d9dd
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-02-01 21:59:41 +0100

    refactor Categories component to be more reuseable

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -7,11 +7,50 @@ import { MdDeveloperBoard } from 'react-icons/md';
 import Category from './Category';
 
 const Categories = ({ category }) => {
+  const categories = [
+    {
+      name: 'All products',
+      icon: <TiThSmall size="3vh" />,
+      isBolded: category ? false : true,
+      path: '/store',
+    },
+    {
+      name: 'CPUs',
+      icon: <BsCpu size="3vh" />,
+      isBolded: category === 'cpu' ? true : false,
+      path: '/store/cpu',
+    },
+    {
+      name: 'Memory',
+      icon: <FaMemory size="3vh" />,
+      isBolded: category === 'ram' ? true : false,
+      path: '/store/ram',
+    },
+    {
+      name: 'Monitors',
+      icon: <FaDesktop size="3vh" />,
+      isBolded: category === 'monitor' ? true : false,
+      path: '/store/monitor',
+    },
+    {
+      name: 'Graphic Cards',
+      icon: <BsCpuFill size="3vh" />,
+      isBolded: category === 'gpu' ? true : false,
+      path: '/store/gpu',
+    },
+    {
+      name: 'Motherboards',
+      icon: <MdDeveloperBoard size="3vh" />,
+      isBolded: category === 'motherboard' ? true : false,
+      path: '/store/motherboard',
+    },
+  ];
+
   return (
     <Box w={{ base: '100%', sm: '100%', md: '250px', lg: '300px' }}>
       <Text
         fontWeight="bold"
-        mb={2}
+        mb={3}
         pl="2%"
         fontSize={{ base: '20px', md: '24px', lg: '30px' }}
       >
@@ -19,47 +58,19 @@ const Categories = ({ category }) => {
       </Text>
       <VStack
         divider={<StackDivider borderColor="gray.200" />}
-        spacing={2}
+        spacing={{ base: '15px', md: '10px' }}
         align="stretch"
         w="100%"
         mb="3%"
       >
-        <Category
-          name={'All products'}
-          icon={<TiThSmall size="2.2vw" />}
-          isBolded={category ? false : true}
-          path={'/store'}
-        />
-        <Category
-          name={'CPUs'}
-          icon={<BsCpu size="2.2vw" />}
-          isBolded={category === 'cpu' ? true : false}
-          path={'/store/cpu'}
-        />
-        <Category
-          name={'Memory'}
-          icon={<FaMemory size="2.2vw" />}
-          isBolded={category === 'ram' ? true : false}
-          path={'/store/ram'}
-        />
-        <Category
-          name={'Monitors'}
-          icon={<FaDesktop size="2.2vw" />}
-          isBolded={category === 'monitor' ? true : false}
-          path={'/store/monitor'}
-        />
-        <Category
-          name={'Graphics Cards'}
-          icon={<BsCpuFill size="2.2vw" />}
-          isBolded={category === 'gpu' ? true : false}
-          path={'/store/gpu'}
-        />
-        <Category
-          name={'Motherboards'}
-          icon={<MdDeveloperBoard size="2.2vw" />}
-          isBolded={category === 'motherboard' ? true : false}
-          path={'/store/motherboard'}
-        />
+        {categories.map((cat) => (
+          <Category
+            name={cat.name}
+            icon={cat.icon}
+            path={cat.path}
+            isBolded={cat.isBolded}
+          />
+        ))}
       </VStack>
     </Box>
   );
