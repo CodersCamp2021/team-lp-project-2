537493e5c7761d51d2aba23d6cd39ed85852f468
Maciek <maciek.duzy.11@gmail.com>
2022-02-03 17:08:01 +0100

    change Link into navigate

--- src/components/ProductPreview.js
+++ src/components/ProductPreview.js
@@ -1,52 +1,57 @@
 import { Box, Flex, Heading, Text, Image, Button } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { AddIcon } from '@chakra-ui/icons';
 
 const ProductPreview = ({ product }) => {
-  const handleAddToCart = (id) => {
-    console.log(`add to cart item with id: ${id}`);
+  const navigate = useNavigate();
+
+  const handleAddToCart = () => {
+    console.log(`add to cart item with id: ${product.id}`);
+  };
+
+  const handleNavigateToProduct = () => {
+    navigate(`/store/product/${product.id}`);
   };
 
   return (
-    <Link to={`/store/product/${product.id}`}>
-      <Flex
-        p={5}
-        direction="column"
+    <Flex
+      onClick={handleNavigateToProduct}
+      p={5}
+      direction="column"
+      justifyContent="center"
+      gap="5px"
+      shadow="md"
+      borderWidth="1px"
+      borderRadius="10px"
+      width="220px"
+      height="250px"
+      textAlign="center"
+    >
+      <Box
+        boxSize="120px"
+        alignSelf="center"
+        display="flex"
         justifyContent="center"
-        gap="5px"
-        shadow="md"
-        borderWidth="1px"
-        borderRadius="10px"
-        width="220px"
-        height="250px"
-        textAlign="center"
       >
-        <Box
-          boxSize="120px"
-          alignSelf="center"
-          display="flex"
-          justifyContent="center"
-        >
-          <Image
-            src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.name}%2F1.jpg?alt=media`}
-            alt="image"
-            height="90%"
-          />
-        </Box>
-        <Flex gap="5px">
-          <Heading fontSize="md" wordBreak="break-all" textAlign="left">
-            {product.name}
-          </Heading>
-          <Button onClick={handleAddToCart}>
-            <AddIcon />
-          </Button>
-        </Flex>
-        <Text fontSize="md" textAlign="left">
-          ${product.price}
-        </Text>
+        <Image
+          src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.name}%2F1.jpg?alt=media`}
+          alt="image"
+          height="90%"
+        />
+      </Box>
+      <Flex gap="5px">
+        <Heading fontSize="md" wordBreak="break-all" textAlign="left">
+          {product.name}
+        </Heading>
+        <Button onClick={handleAddToCart}>
+          <AddIcon />
+        </Button>
       </Flex>
-    </Link>
+      <Text fontSize="md" textAlign="left">
+        ${product.price}
+      </Text>
+    </Flex>
   );
 };
 
