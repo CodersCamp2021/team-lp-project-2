f1e2651146f2e1a4db71b70b7f745da15b6c8027
Maciek <maciek.duzy.11@gmail.com>
2022-02-03 16:31:30 +0100

    restyle draft

--- src/components/ProductPreview.js
+++ src/components/ProductPreview.js
@@ -1,27 +1,39 @@
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, Image, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import React, { useEffect } from 'react';
+import { AddIcon } from '@chakra-ui/icons';
 
 const ProductPreview = ({ product }) => {
   useEffect(() => {
     console.log(product);
-  }, []);
+  }, [product]);
 
   return (
-    <Link key={product.name} to={`/store/product/${product.id}`}>
+    <Link to={`/store/product/${product.id}`}>
       <Flex
         p={5}
-        flexDirection="column"
+        direction="column"
         justifyContent="center"
         shadow="md"
         borderWidth="1px"
+        borderRadius="10px"
         width="200px"
-        height="160px"
+        // height="160px"
         textAlign="center"
       >
-        <Heading fontSize="md">{product.name}</Heading>
+        <Box maxHeight="100%">
+          <Image
+            src={`https://firebasestorage.googleapis.com/v0/b/team-lp-project-2.appspot.com/o/${product.name}%2F1.jpg?alt=media`}
+            alt="image"
+          />
+        </Box>
+        <Flex>
+          <Heading fontSize="md">{product.name}</Heading>
+          <Button>
+            <AddIcon />
+          </Button>
+        </Flex>
         <Text fontSize="md">${product.price}</Text>
-        <Text fontSize="md">{product.details.brand}</Text>
       </Flex>
     </Link>
   );
