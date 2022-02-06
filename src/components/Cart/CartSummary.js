d65fc0222fda8acd2de6ec9e57390e83b202ec67
Burbinox <hubert.grobelny@interia.pl>
2022-02-06 20:45:00 +0100

    add calculate price

--- src/components/Cart/CartSummary.js
+++ src/components/Cart/CartSummary.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Flex, Text } from '@chakra-ui/react';
 import ClearButton from './ClearButton';
 import BuyButton from './BuyButton';
+import { ProductsContex } from '../../components/ProductContex';
 
 const CartSummary = () => {
+  const { state } = useContext(ProductsContex);
+
+  function calculatePrice() {
+    let sum = 0;
+    for (let index = 0; index < state.products.length; index++) {
+      sum = sum + state.products[index].price * state.products[index].amount;
+    }
+    return sum;
+  }
   return (
     <Box py={10} color="#000">
       <Text fontSize="3xl" align="center">
-        Total: $951.93
+        Total: {calculatePrice()}$
       </Text>
       <Flex direction="column" align="center" justify="end">
         <ClearButton />
