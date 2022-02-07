ff79660549f02bc6f59108dc4eef990c3ab801d2
Maciek <maciek.duzy.11@gmail.com>
2022-02-07 20:46:37 +0100

    move comp into MenuLinks

--- src/components/Navigation/MenuLinks.js
+++ src/components/Navigation/MenuLinks.js
@@ -68,6 +68,22 @@ const MenuLinks = ({ isMenuOpen, openCart, closeMenu }) => {
             openCart();
           }}
         />
+        <Flex
+          justify="center"
+          align="center"
+          position="absolute"
+          right="0"
+          bottom="0"
+          transform="translate(60%, 60%)"
+          width="30px"
+          height="30px"
+          zIndex="1000"
+          bg="purple.500"
+          color="whiteAlpha.900"
+          borderRadius="50%"
+        >
+          <Text>5</Text>
+        </Flex>
       </Flex>
     </Stack>
   );
