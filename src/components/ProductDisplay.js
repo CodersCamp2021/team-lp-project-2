5109047b8333791ecde734236b5129be06d52b1d
pastepi <p.stepien@livechat.com>
2022-02-09 14:07:49 +0100

    Implemented UX loading/spinners

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -21,12 +21,13 @@ import { db } from '../firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { ProductContext } from './ProductContext';
 import { AllProductsContext } from './App';
+import { delayLoading } from './utils';
 
 const ProductDisplay = ({ setProductName }) => {
   let { productId } = useParams();
   const products = useContext(AllProductsContext);
   const [productInfo, setProductInfo] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [imageURL, setImageURL] = useState('');
   const [noOfProducts, setNoOfProducts] = useState(1);
@@ -54,8 +55,6 @@ const ProductDisplay = ({ setProductName }) => {
   //getting productInfo object from firebase
   const getProduct = async (productId) => {
     const docRef = doc(db, 'products', productId);
-    console.log(productId);
-    setIsLoading(true);
     setError(null);
 
     const docSnap = await getDoc(docRef);
@@ -68,8 +67,6 @@ const ProductDisplay = ({ setProductName }) => {
     } else {
       setError('Product not found');
     }
-
-    setIsLoading(false);
   };
 
   function handleAddManyProducts() {
@@ -89,19 +86,10 @@ const ProductDisplay = ({ setProductName }) => {
     //eslint-disable-next-line
   }, [productId]);
 
-  if (isLoading) {
-    return (
-      <Flex w="100%" justifyContent="center">
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="purple.500"
-          size="xl"
-        />
-      </Flex>
-    );
-  }
+  useEffect(() => {
+    const timer = delayLoading(setIsLoading, productInfo);
+    return () => clearTimeout(timer);
+  }, [productInfo]);
 
   if (error) {
     return (
@@ -116,7 +104,7 @@ const ProductDisplay = ({ setProductName }) => {
 
   return (
     <Box>
-      {productInfo && (
+      {!isLoading ? (
         <Flex flexDirection="column" flexWrap="wrap">
           <Flex
             direction={{ base: 'column', '2xl': 'row' }}
@@ -211,6 +199,16 @@ const ProductDisplay = ({ setProductName }) => {
               </Flex>
             </Flex>
           </Flex>
+        </Flex>
+      ) : (
+        <Flex w="100%" justifyContent="center">
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="purple.500"
+            size="xl"
+          />
         </Flex>
       )}
     </Box>
